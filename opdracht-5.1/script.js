caaa63938a353da0c004e7f02a2e3ede7388ce8b
Mohamed <144031357+MBouhaddani@users.noreply.github.com>
2023-10-12 09:22:31 +0200

    files Up
    .

---
+++ opdracht-5.1/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var element = document.getElementById('mijnElement');
+    element.style.color = 'red';
+    element.textContent = 'De tekst is veranderd!';
+});
