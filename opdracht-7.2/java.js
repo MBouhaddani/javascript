1bde83238fa7ea9c36086f1b1c51603e5935e3b7
MBouhaddani <9021551@student.zadkine.nl>
2023-10-25 13:19:42 +0200

    v5

---
+++ opdracht-7.2/java.js
@@ -0,0 +1,8 @@
+var productPrijs = 50; 
+var gebruikerGeld = prompt("Hoeveel geld heb je?");
+
+if (gebruikerGeld >= productPrijs) {
+    alert("Je hebt genoeg geld om dit product te kopen!");
+} else {
+    alert("Sorry, je hebt niet genoeg geld om dit product te kopen.");
+}
