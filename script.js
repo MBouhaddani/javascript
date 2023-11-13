418a5af0d5ef9bdbe326acdc1528d13445941bfc
MBouhaddani <9021551@student.zadkine.nl>
2023-11-13 19:46:44 +0100

    b2

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var element = document.getElementById('mijnElement');
-    element.style.color = 'red';
-    element.textContent = 'De tekst is veranderd!';
-});
