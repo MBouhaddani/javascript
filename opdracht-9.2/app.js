c3234330f0eb5d46d33ea1df2bf2159b6e736f90
Mohamed <144031357+MBouhaddani@users.noreply.github.com>
2023-11-13 15:36:32 +0100

    Update app.js

--- opdracht-9.2/app.js
+++ opdracht-9.2/app.js
@@ -1,1 +1,11 @@
-h
+var openEyes = document.getElementById("opened")
+var closeEyes = document.getElementById("closed")
+
+openEyes.onclick = function() {
+openEyes.style.display = "none";
+closeEyes.style.display = "block";
+}
+closeEyes.onclick = function() {
+    openEyes.style.display = "block";
+    closeEyes.style.display = "none";
+    }
